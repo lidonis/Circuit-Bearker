60ab8382ebd8e8958fe4bffb12240e38246f75da
lidonis <lidonis.calhau@gmail.com>
2022-07-23 06:20:00 +0200

    fix test

--- src/test/kotlin/lidonis/fr/circuitbearker/ApplicationTest.kt
+++ src/test/kotlin/lidonis/fr/circuitbearker/ApplicationTest.kt
@@ -1,28 +1,36 @@
 package lidonis.fr.circuitbearker
 
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.resources.*
+import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import lidonis.fr.circuitbearker.plugins.BearsResource
-import lidonis.fr.circuitbearker.plugins.configureRouting
-import lidonis.fr.circuitbearker.plugins.configureSerialization
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
     @Test
     fun `create new bear`() = testApplication {
-        application {
-            configureRouting()
-            configureSerialization()
+        val response = httpClient().post(BearsResource()) {
+            contentType(ContentType.Application.Json)
+            setBody(BearsResource.BearRequest("Baloo"))
         }
 
-       val response = client.post(BearsResource(name = "Baloo"))
-
         response.apply {
-            assertEquals(HttpStatusCode.OK, status)
+            assertEquals(HttpStatusCode.Created, status)
+            assertContains(bodyAsText(),"Baloo")
+        }
+    }
+
+    private fun ApplicationTestBuilder.httpClient() = createClient {
+        install(Resources)
+        install(ContentNegotiation) {
+            json()
         }
     }
 }
