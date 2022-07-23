ddb396672a2aca7e337f7bc3d7adcbdccba1d475
lidonis <lidonis.calhau@gmail.com>
2022-07-23 06:04:18 +0200

    fix routing

--- src/main/kotlin/lidonis/fr/circuitbearker/domain/BearsUseCases.kt
+++ src/main/kotlin/lidonis/fr/circuitbearker/domain/BearsUseCases.kt
@@ -1,12 +1,12 @@
 package lidonis.fr.circuitbearker.domain
 
 import java.util.UUID
+import kotlin.time.Duration
 
 
 interface BearsUseCases {
     data class CreateCommand(val name: String)
-
-    fun create(command: CreateCommand): Bear.BearId
+    fun create(command: CreateCommand): Bear
     fun retrieve(id: Bear.BearId): Bear?
 }
 
@@ -14,13 +14,11 @@ object Bears : BearsUseCases {
 
     private val bearsMap = mutableMapOf<Bear.BearId, Bear>()
 
-    override fun create(command: BearsUseCases.CreateCommand) =
-        Bear.BearId().also {
-            bearsMap[it] = Bear.AwakeBear(
-                id = it,
-                name = command.name,
-            )
-        }
+    override fun create(command: BearsUseCases.CreateCommand): Bear {
+        val bear = Bear.create(command.name)
+        bearsMap[bear.id] = bear
+        return bear
+    }
 
     override fun retrieve(id: Bear.BearId) = bearsMap[id]
 
@@ -30,9 +28,23 @@ object Bears : BearsUseCases {
 interface Bear {
     val id: BearId
     val name: String
-
-    @JvmInline
+            @JvmInline
     value class BearId(val value: UUID = UUID.randomUUID())
 
-    data class AwakeBear(override val id: BearId, override val name: String) : Bear
+    fun hibernate(duration: Duration): Bear = HibernateBear(id, name, duration)
+
+    fun wakeUp(): Bear = AwakeBear(id, name)
+
+    private data class AwakeBear(override val id: BearId, override val name: String) : Bear
+
+    private data class HibernateBear(
+        override val id: BearId,
+        override val name: String,
+        val duration: Duration
+    ) : Bear
+
+    companion object {
+        fun create(name: String): Bear = AwakeBear(BearId(), name)
+    }
 }
+
--- src/main/kotlin/lidonis/fr/circuitbearker/plugins/Routing.kt
+++ src/main/kotlin/lidonis/fr/circuitbearker/plugins/Routing.kt
@@ -1,48 +1,62 @@
 package lidonis.fr.circuitbearker.plugins
 
+import io.ktor.http.*
 import io.ktor.resources.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.resources.*
 import io.ktor.server.resources.Resources
+import io.ktor.server.resources.post
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.routing.post
 import io.ktor.server.routing.put
 import kotlinx.serialization.Serializable
 import lidonis.fr.circuitbearker.domain.Bear
 import lidonis.fr.circuitbearker.domain.Bears
-import lidonis.fr.circuitbearker.domain.BearsUseCases
+import lidonis.fr.circuitbearker.domain.BearsUseCases.CreateCommand
+import lidonis.fr.circuitbearker.plugins.BearsResource.*
 import java.util.*
+
+@Serializable
+@Resource("/bears")
+class BearsResource {
+    @Serializable
+    data class BearRequest(val name: String)
+
+    @Serializable
+    data class BearBody(val id: String, val name: String)
+
+    @Serializable
+    @Resource("{id}")
+    class Id(val parent: BearsResource, val id: String) {
+
+        @Serializable
+        @Resource("hibernate")
+        class Hibernate(val parent: BearsResource.Id, val id: String)
+    }
+}
 
 fun Application.configureRouting() {
     install(Resources)
 
     routing {
-        post<BearsResource> { bear ->
-            call.respond(Bears.create(BearsUseCases.CreateCommand(bear.name)))
+        post<BearsResource> {
+            val bearRequest = call.receive<BearRequest>()
+            val bear = Bears.create(CreateCommand(bearRequest.name))
+            call.response.headers.append(HttpHeaders.Location, "/bears/${bear.id.value}")
+            call.respond(
+                status = HttpStatusCode.Created,
+                BearBody(id = bear.id.value.toString(), name = bear.name)
+            )
         }
 
-        get<BearsResource.Id> { bearId ->
-            call.respond(Bears.retrieve(Bear.BearId(UUID.fromString(bearId.id))) ?: error("No bear"))
+        get<Id> { bearId ->
+            val bear = Bears.retrieve(Bear.BearId(UUID.fromString(bearId.id))) ?: error("No bear")
+            call.respond(BearBody(id = bear.id.value.toString(), name = bear.name))
         }
 
-        put<BearsResource.Id.Hibernate> { status ->
+        put<Id.Hibernate> { status ->
             call.respondText("hibernate bear ${status.id}")
         }
     }
 }
-
-@Serializable
-@Resource("/bears")
-class BearsResource(val name: String) {
-
-    @Serializable
-    @Resource("{id}")
-    class Id(val id: String) {
-
-        @Serializable
-        @Resource("hibernate")
-        class Hibernate(val id: String)
-    }
-}
-
